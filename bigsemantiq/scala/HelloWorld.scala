41074402f959b87648f3fef29c38fc570fe30913
John <j.kvarnefalk@gmail.com>
2020-11-22 19:50:03 +0100

    Basic sql functions working

--- bigsemantiq/scala/HelloWorld.scala
+++ bigsemantiq/scala/HelloWorld.scala
@@ -12,31 +12,55 @@ import com.google.zetasql.{
   SimpleColumn,
   SimpleTable,
   SimpleType,
-  TypeFactory
+  TypeFactory,
+  ZetaSQLBuiltinFunctionOptions
 }
 
 object HelloWorld {
   def main(args: Array[String]) = {
-    val tableName = "user_table";
-    val sqlStatement = s"SELECT user_id FROM ${tableName}"
+    val tableName = "table1";
+    val table2 = "table2";
+    val sqlStatement = s"SELECT MAX(user_id) FROM ${tableName}"
+    val sql =
+      "SELECT table1.random_stuff \nFROM table1\nINNER JOIN table2\nON table1.column_name = table2.column_name;"
     val simpleCatalog = new SimpleCatalog("global")
+    simpleCatalog.addZetaSQLFunctions(new ZetaSQLBuiltinFunctionOptions());
     simpleCatalog.addSimpleTable(
       createSimpleTable(
         tableName,
         List(
           createSimpleColumn(
             tableName,
-            "user_id",
+            "column_name",
             TypeFactory.createSimpleType(TypeKind.TYPE_INT64)
+          ),
+          createSimpleColumn(
+            tableName,
+            "random_stuff",
+            TypeFactory.createSimpleType(TypeKind.TYPE_BOOL)
           )
         )
       )
     )
-    val statement: ResolvedNodes.ResolvedStatement = Analyzer.analyzeStatement(
-      sqlStatement,
-      new AnalyzerOptions(),
-      simpleCatalog
+    simpleCatalog.addSimpleTable(
+      createSimpleTable(
+        table2,
+        List(
+          createSimpleColumn(
+            table2,
+            "column_name",
+            TypeFactory.createSimpleType(TypeKind.TYPE_INT64)
+          ),
+          createSimpleColumn(
+            table2,
+            "other_name",
+            TypeFactory.createSimpleType(TypeKind.TYPE_STRING)
+          )
+        )
+      )
     )
+    val statement: ResolvedNodes.ResolvedStatement =
+      Analyzer.analyzeStatement(sql, new AnalyzerOptions(), simpleCatalog)
     val a = 5
   }
 
